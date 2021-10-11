c2869ec66b220dc3ec16088209e71d9e17524039
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-10-11 15:24:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# by Lucas Cavalcante, 2021
+
+"""
+This script moves all files from all members of a group to an `archive`
+folder.
+The name of the group is be a parameter of the program.
+
+Change the targe directory setting the `TARGETDIR` environment variable.
+
+Example:
+
+>>> python main.py --groupname {GROUP_NAME}
+"""
+
+from argparse import ArgumentParser
+from grp import getgrall
+from subprocess import call, Popen, PIPE
+import logging
+import os
+
+_log = logging.getLogger(__name__)
+logging.basicConfig(filename='out.log', level=logging.DEBUG)
+
+TARGETDIR = os.getenv('TARGETDIR', 'archive')
+
+
+def get_group_members(groupname: str) -> list:
+    """
+    Get all user members of a group.
+    """
+    group_members = [
+        group.gr_mem for group in getgrall() if group.gr_name == groupname
+    ]
+    _log.info(f" [*] Obtained user members of group '{groupname}'")
+    return next(iter(group_members), [])
+
+
+def move_files(files: list) -> None:
+    """
+    Move all files from a list of files to the `TARGETDIR` directory.
+    """
+    if not os.path.exists(TARGETDIR):
+        os.mkdir(TARGETDIR)
+        _log.info(f" [*] Directory '{TARGETDIR}' created")
+    for file in files:
+        try:
+            call(f'mv {file} {TARGETDIR}', shell=True)
+            _log.info(f" [*] Moved '{file}' to directory '{TARGETDIR}'")
+        except Exception as exc:
+            _log.error(
+                ' [!] Could not move file {file}. Task raised exception: %r'
+                % (file, exc)
+            )
+            raise exc
+
+
+def get_all_files_owned_by_user(username: str) -> list:
+    """
+    Get all files owned by an user in the filesystem.
+    """
+    try:
+        proc = Popen(f'find / -user {username} 2>&-', shell=True, stdout=PIPE)
+        output = proc.stdout.read()
+        files = output.decode('utf-8').splitlines()
+        _log.info(f" [*] Obtained list of files owned by user '{username}'")
+        return files
+    except Exception as exc:
+        _log.error(
+            ' [!] Could not get files owned by %r. Task raised exception: %r'
+            % (username, exc)
+        )
+        raise exc
+
+
+def move_files_of_an_owner(username: str) -> None:
+    """
+    Move all files in the filesystem owned by an user to the
+    `TARGETDIR` directory
+    """
+    files = get_all_files_owned_by_user(username)
+    try:
+        move_files(files)
+        _log.info(f" [*] Successfully moved all files owned by user '{username}'")
+    except Exception as exc:
+        _log.error(
+            ' [!] Could not move files owned by %r. Task raised exception: %r'
+            % (username, exc)
+        )
+        raise exc
+
+
+def move_files_of_a_group(groupname_value: str) -> None:
+    group_members = get_group_members(groupname_value)
+    for user in group_members:
+        move_files_of_an_owner(user)
+
+
+if __name__ == '__main__':
+    """
+    Main script function
+    """
+    parser = ArgumentParser(description='Enter target group.')
+    parser.add_argument('--groupname', type=str, default='')
+    args = parser.parse_args()
+
+    groupname_value = args.groupname
+    move_files_of_a_group(groupname_value)
+
+    # group_members = get_group_members(groupname_value)
+    # for user in group_members:
+    #      move_files_of_an_owner(user)
+    # files = get_all_files_owned_by_user(user)
+    # move_files(files)
+
+
+# def get_source_files(dirpath: str) -> list:
+#     return [
+#         os.path.join(os.path.abspath(dirpath), file) for file in os.listdir(dirpath)
+#     ]
+
+
+# def get_file_owner(filepath: str) -> str:
+#     return getpwuid(os.stat(filepath).st_uid).pw_name
+
+
+# def get_selected_files(files_and_owners: list, group_members: list) -> list:
+#     return [file for file, owner in files_and_owners if owner in group_members]
---
+++ mem.md
@@ -0,0 +1,26 @@
+# Task
+
+**Implementation of a Python script for archieving files on a Linux system**
+
+A Python script should be implemented which moves all files from all members of a group to an archive folder. The name of the group should be a parameter of the program.
+
+The program should be robust, e.g. with respect to multiple invocations in short time. The events and results should be available in a log file.
+
+The program should be installable as a Debian package and should be made available for download. The Debian package does not have to be built, the sources are sufficient.
+
+##
+
+Author: Lucas Cavalcante, 2021
+
+## References
+
+- https://stackoverflow.com/questions/9323834/python-how-to-get-group-ids-of-one-username-like-id-gn
+
+- https://www.digitalocean.com/community/tutorials/how-to-add-and-delete-users-on-ubuntu-18-04
+- https://www.techrepublic.com/article/how-to-create-users-and-groups-in-linux-from-the-command-line/
+
+- https://docs.oracle.com/cd/E19683-01/817-0365/secfile-3/index.html
+
+- https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+
+- https://stackoverflow.com/questions/1830618/how-to-find-the-owner-of-a-file-or-directory-in-python
